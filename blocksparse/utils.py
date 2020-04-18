c60fcfa7bcf8b502c80d30e40162f221a0aafb8e
temp2334 <63928168+temp2334@users.noreply.github.com>
2020-04-18 15:38:41 -0400

    Update utils.py

--- blocksparse/utils.py
+++ blocksparse/utils.py
@@ -12,7 +12,7 @@ from operator import mul
 if sys.version_info >= (3, 0):
     from functools import reduce
 
-data_files_path = tf.resource_loader.get_data_files_path()
+data_files_path = tf.compat.v1.resource_loader.get_data_files_path()
 _op_module = tf.load_op_library(os.path.join(data_files_path, 'blocksparse_ops.so'))
 # for x in dir(_op_module):
 #     print(x)
